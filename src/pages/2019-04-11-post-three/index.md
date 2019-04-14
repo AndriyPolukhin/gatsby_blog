180f3d14585023c479ff378e340578cdd132bebd
AndriyPolukhin <andriy.polukhin@gmail.com>
2019-04-14 12:47:33 +0300

    gatsby blog

---
+++ src/pages/2019-04-11-post-three/index.md
@@ -0,0 +1,8 @@
+---
+path: "/post-three"
+date: "2019-04-11"
+title: "My Third Gatsby Post"
+author: "Andriy Polukhin"
+---
+
+This is my very third blog post in Gatsby
---
+++ src/pages/services.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const ServicesPage = () => (
+  <Layout>
+    <h1>Services Page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,
+      incidunt voluptates consequatur odio id illum recusandae labore aspernatur
+      facilis maiores nulla modi deleniti, explicabo mollitia sunt assumenda
+      earum doloribus molestiae.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,
+      incidunt voluptates consequatur odio id illum recusandae labore aspernatur
+      facilis maiores nulla modi deleniti, explicabo mollitia sunt assumenda
+      earum doloribus molestiae.
+    </p>
+  </Layout>
+)
+
+export default ServicesPage
